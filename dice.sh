216a5c1016d3841b8d69bc47e93d144a9445f59c
Anwesha Das <anwesha@das.community>
2018-08-13 10:29:41 +0530

    Revised code using shuf

--- dice.sh
+++ dice.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-num1=$(( $RANDOM % 7 )); echo "First dice rolled - $num1"
-num2=$(( $RANDOM % 7 )); echo "Second dice rolled - $num2"
+num1=$(shuf -i 1-6 -n 1) 
+echo "First dice rolled - $num1"
+num2=$(shuf -i 1-6 -n 1) 
+echo "Second dice rolled - $num2"
 sum=$((num1+ num2))
 echo "The sum is = $sum"
